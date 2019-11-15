55054f864fbbe17d2fdec139dcded21f993d01ed
YushengFeng01 <Yusheng.Feng@clarivate.com>
2019-11-15 10:55:19 +0800

    add an tool for extracting fields from xml

--- viewers/extract_tool.py
+++ viewers/extract_tool.py
@@ -22,9 +22,10 @@ class ExtractTool(object):
                     tree = etree.parse(StringIO.StringIO(i))
                     elements = tree.xpath(xpath)
                     if len(elements):
-                        text = elements[0].text
-                        text = text.encode('utf-8') if isinstance(text, unicode) else text
-                        csv_writer.writerow([text, path])
+                        for e in elements:
+                            text = e[0].text
+                            text = text.encode('utf-8') if isinstance(text, unicode) else text
+                            csv_writer.writerow([text, path])
 
     def xml_paths(self, path):
         p = os.path.normpath(path)
@@ -39,4 +40,4 @@ if __name__ == '__main__':
     tool = ExtractTool()
     # tool.nodes("D:\\dev\\clean-tool\\ses-1372\\XML\\batch12\\collection=ARCI\\part-0.gz",
     #            "/REC/static_data/summary/titles/title[@transliterated=\'Y\'][@type=\'item\']")
-    tool.xml_paths("D:\\dev\\clean-tool\\build_new_\\build\\XML.txt")
+    tool.xml_paths("/opt/reuters/data/elasticsearch/arci_rosette/diacriticals/clean-tool/build/498b-20191101040805.txt")
